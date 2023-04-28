ff6460ee97801b6428460343b87dbfcc2aed8cc6
Frafaelrls <frafaelrls@gmail.com>
2023-04-28 08:58:23 -0300

    refatorando metodo post para usar a API fetch

--- aluraframe/client/js/app/services/HttpService.js
+++ aluraframe/client/js/app/services/HttpService.js
@@ -1,7 +1,7 @@
 class HttpService {
 
     #handleErrors(res) {
-        if(!res.ok) throw new Error(res.statusText);
+        if (!res.ok) throw new Error(res.statusText);
         return res;
     }
 
@@ -14,29 +14,11 @@ class HttpService {
 
     post(url, dado) {
 
-
-        return new Promise((resolve, reject) => {
-
-            let xhr = new XMLHttpRequest();
-            xhr.open("POST", url, true);
-            xhr.setRequestHeader("Content-Type", "application/json");
-
-            xhr.onreadystatechange = () => {
-
-                if (xhr.readyState == 4) {
-
-                    if (xhr.status == 200) {
-
-                        resolve(JSON.parse(xhr.responseText));
-                    } else {
-
-                        reject(xhr.responseText);
-                    }
-                }
-            };
-
-            xhr.send(JSON.stringify(dado));
-        });
-
+        return fetch(url, {
+            headers: { 'Content-Type': "application/json" },
+            method: 'POST',
+            body: JSON.stringify(dado)
+        })
+        .then(res => this.#handleErrors(res))
     }
 }
